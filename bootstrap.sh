145c9eddcb3f00987fe1286eeaff063610cc3b88
Ian Keen <iankeen82@gmail.com>
2018-07-11 16:08:48 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 
 echo "Configuring development environment"
 echo ""
+echo "This may take some time... grab a coffee."
+echo ""
 echo "Before continuing ensure you have Xcode installed and you have opened it and accepted the User Agreement"
 read -p "then press [Return] to continue..."
 
@@ -13,28 +15,29 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Ensure latest xcode tools
 echo "Installing Xcode command line tools..."
-xcode-select --install
+xcode-select --install >/dev/null
 
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >/dev/null
 fi
 
+echo "Updating brew and casks"
 # Make sure we’re using the latest Homebrew.
-brew update
+brew update >/dev/null
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade >/dev/null
 
 echo "Installing git..."
-brew install git
+brew install git >/dev/null
 
 # Install ruby-build and rbenv
 echo "Installing rbenv..."
-brew install ruby-build
-brew install rbenv
+brew install ruby-build >/dev/null
+brew install rbenv >/dev/null
 RBENVINIT='eval "$(rbenv init -)"'
 if [[ $SHELL =~ "bash" ]]; then
     touch ~/.bash_profile
@@ -51,18 +54,18 @@ else
 fi
 
 echo "Configuring rbenv..."
-rbenv install 2.5.0
-rbenv global 2.5.0
-rbenv rehash
+rbenv install 2.5.0 >/dev/null
+rbenv global 2.5.0 >/dev/null
+rbenv rehash >/dev/null
 
 echo "Installing Cocoapods..."
-gem install cocoapods
+gem install cocoapods >/dev/null
 
 echo "Rehashing..."
-rbenv rehash
+rbenv rehash >/dev/null
 
 echo "Installing bundler..."
-gem install bundler
-bundle install
+gem install bundler >/dev/null
+bundle install >/dev/null
 
 echo "Complete."
